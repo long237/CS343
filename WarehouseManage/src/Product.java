6d8f4011c2f5524b60caf315e7dc8e15c6f7c71a
keirakaitlynn <keirakaitlynn@gmail.com>
2020-11-06 12:33:55 -0800

    Set up constructor & private variables.

--- WarehouseManage/src/Product.java
+++ WarehouseManage/src/Product.java
@@ -1,16 +1,37 @@
-// XXXXX: (fix ASAP!)
-// KEIRA: (main headers)
-// kkkkk: (sub-headers)
-// KKKKK: (sub sub-headers)
+
+// XXXXX: (fix ASAP!)                (#ff0070)
+// KEIRA: (main headers)             (#ff0070)
+// kkkkk: (sub-headers)              (#fcb9c5)
+// KKKKK: (sub sub-headers)          (#b97474)
 //
-// TODO: (incomplete)
-// QUESTION: (wtf did i just do)
+// TODO: (incomplete)                (#ccff00)
+//# QUESTION: (wtf did i just do)    (#00b9ff)
+
+import java.util.HashSet;
 
 public class Product {
-    public static void main(String[] args) {
-        System.out.println("Hello Worldio");
-        System.out.println("helloooooooooooooooo");
-        System.out.println("Hello World");
-        System.out.println();
+
+    private String mName;
+    private int mQuantityAvailable;
+    private double mPrice;
+    private int mAmountSold;
+
+    //private double mTotalSales;
+    //private double mTotalCost;
+    //private double mTotalProfit;
+    //private double mProfitPercent;
+
+    public Product() {
+        this.mName = "john doe";
+        this.mQuantityAvailable = 1234;
+        this.mPrice = 0.0;
+        this.mAmountSold = 1234;
+    }
+
+    public Product(String mName, int mQuantityAvailable, double mPrice, int mAmountSold) {
+        this.mName = mName;
+        this.mQuantityAvailable = mQuantityAvailable;
+        this.mPrice = mPrice;
+        this.mAmountSold = mAmountSold;
     }
 }
