adbbde4e7bb1c89917d6d3e475cc7b119feee884
keirakaitlynn <keirakaitlynn@gmail.com>
2020-11-06 10:26:44 -0800

    comments

--- WarehouseManage/src/Product.java
+++ WarehouseManage/src/Product.java
@@ -1,3 +1,11 @@
+
+// XXXXX: (fix ASAP!)
+// KEIRA: (main headers)
+// kkkkk: (sub-headers)
+// KKKKK: (sub sub-headers)
+//
+// TODO: (incomplete)
+//# QUESTION: (wtf did i just do)
+
 public class Product {
-    //Beginning of our code here
 }
