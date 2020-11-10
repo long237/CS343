cddc9dbc3fa15c4b9e45460782d69aa50a377d55
Brian Vuong <AlphaXBrian@yahoo.com>
2020-11-09 23:10:23 -0800

    salesperson prototype (salespeople as a package so that we can call the class salesperson

---
+++ WarehouseManage/src/Salespeople/Salesperson.java
@@ -0,0 +1,39 @@
+package Salespeople;
+
+public class Salesperson {
+    private String mSalespersonName;
+    private double mSalespersonCommission;
+    private int mSalespersonID;
+    private int mTotalSales;
+    public String getSalespersonName() {
+        return mSalespersonName;
+    }
+
+    public void setSalespersonName(String mSalespersonName) {
+        this.mSalespersonName = mSalespersonName;
+    }
+
+    public double getSalespersonCommission() {
+        return mSalespersonCommission;
+    }
+
+    public void setSalespersonCommission(double mSalespersonCommission) {
+        this.mSalespersonCommission = mSalespersonCommission;
+    }
+
+    public int getSalespersonID() {
+        return mSalespersonID;
+    }
+
+    public void setSalespersonID(int mSalespersonID) {
+        this.mSalespersonID = mSalespersonID;
+    }
+
+    public int getTotalSales() {
+        return mTotalSales;
+    }
+
+    public void setTotalSales(int mTotalSales) {
+        this.mTotalSales = mTotalSales;
+    }
+}
